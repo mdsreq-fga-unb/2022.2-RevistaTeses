97d253186dc42c5af5ace6338b49c9287b9a253a
João Pedro Morbeck <jpmorbeck45@gmail.com>
2022-12-13 09:01:23 -0300

    Configuração inicial do backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
