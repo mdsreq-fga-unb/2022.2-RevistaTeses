c4cf6c1768e7f0ad2c45b64e8e5b072cc6c7fced
PedroAbreu <pedroabreupmab@gmail.com>
2022-12-17 20:16:05 -0300

    Criando HomePage

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -2,18 +2,38 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-        <h1>HomePagina</h1>
-        <header>
-          <ul class="navbar-items">
-            <li class="navbar-item">Premium</li>
-            <li class="navbar-item">Ajuda</li>
-            <li class="navbar-item">Baixar</li>
-            <li class="navbar-item">Inscrever-se</li>
-            <li class="navbar-item">Entrar</li>
-          </ul>
-        </header>
-    </div>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Portal Teses</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+
+  <body>
+    <header>
+      <nav>
+        <a class="logo" href="#">Portal Teses</a>
+        <div class="mobile-menu">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+
+        <ul class="nav-list">
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Colunas</a></li>
+          <li><a href="#">Notícias</a></li>
+          <li><a href="#">Eventos</a></li>
+          <li><a href="#">Entrar</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main></main>
+    <script src="mobile-navbar.js"></script>
+  </body>
+</html>
   )
 }
 
