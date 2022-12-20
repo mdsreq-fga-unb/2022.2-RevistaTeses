85f90de369c81bb27b68db143dda2583cd9eb3bb
Ana Carolina Costa Cesar <anaipva@gmail.com>
2022-12-20 15:42:17 -0300

    Parte do front de login

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -8,25 +8,19 @@ const Login = () => {
         <div>
         <body>
             <div className='container'>
-                <h1>
-                    cade o container?
-                </h1>
-                <div>
-                <button type='submit'>Entrar</button>
-                </div>
-                {/* <form className='form'>
+                <form>
                 <div className='field'>
                     <label htmlFor='email'>Email</label>
-                    <input type="email" name='email' id='email'> </input>
+                    <input type="email" name='email' id='email'/> 
                 </div>
                 <div className='field'>
                     <label htmlFor='password'>Senha</label>
-                    <input type="password" name='password' id='password'> </input>
+                    <input type="password" name='password' id='password'/>
                 </div>
                 <div className='actions'>
                     <button type='submit'>Entrar</button>
                 </div>
-                </form> */}
+                </form> 
             </div>
         </body>
         </div>
