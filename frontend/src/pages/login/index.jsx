dcd55a46d527aceeea3a9beef7c5b8fd9be8cba4
PedroAbreu <pedroabreupmab@gmail.com>
2023-01-09 09:57:41 -0300

    Atualização da página de login

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -4,27 +4,23 @@ import "../login/styles.css";
 const Login = () => {
   return (
     <div id="login">
-      <h1 className="title">Portal Teses</h1>
-      <div>
-        <background />
-        <div className="container">
-          <h2 className="subtitle">Faça seu login</h2>
-          <form action="/auth/login" method="post">
-            <div className="field">
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" required />
-            </div>
-            <div className="field">
-              <label htmlFor="password">Senha</label>
-              <input type="password" name="password" id="password" required />
-            </div>
-            <button type="submit" className="button">
-              Entrar
-            </button>
-          </form>
-          <h3 className="subtitle">Ainda não tem uma conta? Cadastre-se</h3>
+      <h2 className="login">Faça seu login</h2>
+
+      <form id="form" action="/auth/login" method="post">
+        <div id="campoEmail" class="campoEmail">
+          <label htmlFor="email"><b>Email</b></label>
+          <input type="email" name="email" id="email" required />
         </div>
-      </div>
+
+        <div id="campoSenha" class="campoSenha">
+          <label htmlFor="password"><b>Senha</b></label>
+          <input type="password" name="password" id="password" required />
+        </div>
+
+        <button type="submit" className="botaoEntrar">Entrar</button>
+      </form>
+
+      <h3 className="cadastro">Ainda não tem uma conta? <a href="#">Cadastre-se aqui!</a></h3>
     </div>
   );
 };
