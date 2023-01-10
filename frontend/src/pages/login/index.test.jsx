01c11cf138af7acbb5240e513026c12299521316
Ana Carolina Costa Cesar <anaipva@gmail.com>
2023-01-10 17:29:41 -0300

    Colocando jest

---
+++ frontend/src/pages/login/index.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Login from '.';
+
+describe('Ver se a pagina de login ta funcionando', () => {
+    test('renderiza sem quebrar', () => {
+        second })
+});
