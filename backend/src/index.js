447bded6c650ed153571a43baa5b03d997742522
João Pedro Morbeck <jpmorbeck45@gmail.com>
2023-01-12 13:46:11 -0300

    Update .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONT,
   credentials: true,
   exposedHeaders: ['set-cookie']
 }));
